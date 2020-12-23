948ba88bcc8fa7243979f92165deb228ce144d1d
Laura Jimenez <lauraisa1298@hotmail.com>
2020-12-23 14:51:12 -0500

    Se agrego  r y q con 50mil de tope

--- GEstandarMinimo.py
+++ GEstandarMinimo.py
@@ -9,15 +9,26 @@ print("")
 m=int (input("Por favor ingresa el modulo: "))
 print("")
 
-q=np.floor(m/a)
-q=int(q)
+decision= input("Desea ingresar el r y q")
+if decision == "si":
+    q=int (input("Por favor ingresa q: "))
+    r=int (input("Por favor ingresa r: "))
+else:
+    q=np.floor(m/a)
+    q=int(q)
 
-r=m%a
+    r=m%a
+
 Rn=[]
 Almacen=[x]
 while True: 
-    Xn1= (a*x)%m
-    
+    Xn1= a*(x%q)-r* np.floor(x/q)
+    if len(Almacen)==50000:
+        break
+   
+    if Xn1 < 0:
+        Xn1+=m
+
     if Xn1 in Almacen:
         break
     else: 
